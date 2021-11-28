4ed1cc8fb21d36cd757391d6e2889eaff965cd29
atabiksf <atabik.shofi@gmail.com>
2021-11-28 23:27:15 +0700

    add fish.pl

---
+++ src/fish.pl
@@ -0,0 +1,60 @@
+% Cek posisi player
+isCanFish :- 
+	player(X,Y), W is Y-1, tile_water(X,W), !;
+	player(X,Y), S is Y+1, tile_water(X,S), !;
+	player(X,Y), A is X-1, tile_water(A,Y), !;
+	player(X,Y), D is X+1, tile_water(D,Y).
+
+% Belum add EXP
+addCatfish :-
+	retract(item(1,catfish,X,75,0,0)), NewX is X+1, assertz(item(1,catfish,NewX,75,0,0)).
+addSalmon :-
+	retract(item(2,salmon,X,150,0,0)), NewX is X+1, assertz(item(2,salmon,NewX,150,0,0)).
+addTuna :-
+	retract(item(3,tuna,X,250,0,0)), NewX is X+1, assertz(item(3,tuna,NewX,250,0,0)).
+
+% Kondisi Fishing tiap level
+fish1 :- 
+	random(1,101,N),
+	(	N > 0, N =< 50 -> write('Gadapet apa-apa bos!'), nl, write('Dapet 10 xp bos!'), nl;
+		N > 50, N =< 100 -> write('Selamat bos! dapet lele'), nl, write('Dapet 20 xp bos!'), nl, addCatfish
+	).
+fish2 :-
+	random(1,101,N),
+	(	N > 0, N =< 45 -> write('Gadapet apa-apa bos!'), nl, write('Dapet 10 xp bos!'), nl;
+		N > 45, N =< 85 -> write('Selamat bos! dapet lele'), nl, write('Dapet 20 xp bos!'), nl, addCatfish;
+		N > 85, N =< 100 -> write('Selamat bos! dapet salmon'), nl, write('Dapet 35 xp bos!'), nl, addSalmon
+	).
+fish3 :-
+	random(1,101,N),
+	(	N > 0, N =< 40 -> write('Gadapet apa-apa bos!'), nl, write('Dapet 10 xp bos!'), nl;
+		N > 40, N =< 80 -> write('Selamat bos! dapet lele'), nl, write('Dapet 20 xp bos!'), nl, addCatfish;
+		N > 80, N =< 100 -> write('Selamat bos! dapet salmon'), nl, write('Dapet 35 xp bos!'), nl, addSalmon
+	).
+fish4 :-
+	random(1,101,N),
+	(	N > 0, N =< 30 -> write('Gadapet apa-apa bos!'), nl, write('Dapet 10 xp bos!'), nl;
+		N > 30, N =< 70 -> write('Selamat bos! dapet lele'), nl, write('Dapet 20 xp bos!'), nl, addCatfish;
+		N > 70, N =< 95 -> write('Selamat bos! dapet salmon'), nl, write('Dapet 35 xp bos!'), nl, addSalmon;
+		N > 95, N =< 100 -> write('Selamat bos! dapet tuna'), nl, write('Dapet 50 xp bos!'), nl, addTuna
+	).
+fish5 :-
+	random(1,101,N),
+	(	N > 0, N =< 25 -> write('Gadapet apa-apa bos!'), nl, write('Dapet 10 xp bos!'), nl;
+		N > 25, N =< 65 -> write('Selamat bos! dapet lele'), nl, write('Dapet 20 xp bos!'), nl, addCatfish;
+		N > 65, N =< 90 -> write('Selamat bos! dapet salmon'), nl, write('Dapet 35 xp bos!'), nl, addSalmon;
+		N > 90, N =< 100 -> write('Selamat bos! dapet tuna'), nl, write('Dapet 50 xp bos!'), nl, addTuna
+	).
+
+% Main fish (belum add waktu)
+fish :-
+	isCanFish, !,
+	level(X),
+	(	X == 1 -> fish1;
+		X == 2 -> fish2;
+		X == 3 -> fish3;
+		X == 4 -> fish4;
+		X == 5 -> fish5
+	).
+
+fish :- write('Nggak ada air bos!'), nl.
