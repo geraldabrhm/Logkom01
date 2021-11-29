eb181dd9e07e7026b94737e9f203dcb79c5efa87
DaffaRomyz <daffaromyzaufa@gmail.com>
2021-11-29 11:06:01 +0700

    add player.pl

---
+++ src/player.pl
@@ -0,0 +1,119 @@
+:- dynamic(lvlRanching/1).
+:- dynamic(lvlFishing/1).
+:- dynamic(lvlFarming/1).
+
+
+% inisiasi level
+lvlRanching(1).
+lvlFishing(1).
+lvlFarming(1).
+
+% inisiasi exp
+expRanching(0).
+expFishing(0).
+expFarming(0).
+
+% Level up Ranching
+lvlRanching(2) :-
+    lvlRanching(1),
+    expRanching(X),
+    X >= 300,
+    resetExpRanching.
+
+lvlRanching(3) :-
+    lvlRanching(2),
+    expRanching(X),
+    X >= 500,
+    resetExpRanching.  
+
+lvlRanching(4) :-
+    lvlRanching(3),
+    expRanching(X),
+    X >= 1000,
+    resetExpRanching.  
+
+lvlRanching(5) :-
+    lvlRanching(4),
+    expRanching(X),
+    X >= 1500,
+    resetExpRanching. 
+
+% Level up Fishing
+lvlFishing(2) :-
+    lvlFishing(1),
+    expFishing(X),
+    X >= 300,
+    resetExpFishing.
+
+lvlFishing(3) :-
+    lvlFishing(2),
+    expFishing(X),
+    X >= 500,
+    resetExpFishing.  
+
+lvlFishing(4) :-
+    lvlFishing(3),
+    expFishing(X),
+    X >= 1000,
+    resetExpFishing.  
+
+lvlFishing(5) :-
+    lvlFishing(4),
+    expFishing(X),
+    X >= 1500,
+    resetExpFishing.  
+
+% Level up Farming
+lvlFarming(2) :-
+    lvlFarming(1),
+    expFarming(X),
+    X >= 300,
+    resetExpFarming.
+
+lvlFarming(3) :-
+    lvlFarming(2),
+    expFarming(X),
+    X >= 500,
+    resetExpFarming.  
+
+lvlFarming(4) :-
+    lvlFarming(3),
+    expFarming(X),
+    X >= 1000,
+    resetExpFarming.  
+
+lvlFarming(5) :-
+    lvlFarming(4),
+    expFarming(X),
+    X >= 1500,
+    resetExpFarming.
+
+% reset exp
+expRanching(0) :-
+    resetExpRanching.
+
+expFishing(0) :-
+    resetExpFishing.
+
+expFarming(0) :-
+    resetExpFarming.
+
+% add exp
+expRanching(X) :-
+    addExpRanching(Y),
+    expRanching(X1)
+    X is X1 + Y.
+
+expFishing(X) :-
+    addExpFishing(Y),
+    expFishing(X1)
+    X is X1 + Y.
+
+expFarming(0) :-
+    addExpFarming(Y),
+    expFarming(X1)
+    X is X1 + Y.
+
+addExpRanching(Y) :-
+addExpFishing(Y) :-
+addExpFarming(Y) :-
