015e50ba64c2cbe12c17a84c5d43ccf748143f04
DaffaRomyz <daffaromyzaufa@gmail.com>
2021-11-29 12:01:17 +0700

    add fail state

--- src/house.pl
+++ src/house.pl
@@ -3,6 +3,15 @@ isInHouse :-
 
 sleep :- 
     isInHouse, !,
+    days(X),
+    playerMoney(Y),
+    X =:= 365,
+    Y < 20000,
+    write('You have worked hard, but in the end result is all that matters.'), nl,
+    write('May God bless you in the future with kind people!'), nl.
+
+sleep :- 
+    isInHouse, !,
     addDay(1),
     setHourDawn,
     displayTime,
